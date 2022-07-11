96f641eb9e637294137d3dc77e529bde4ba1ffaa
toschroe <tomke.schroer@cern.ch>
2022-07-11 16:33:24 +0200

    for solving mc

--- preprocessing.py
+++ preprocessing.py
@@ -45,29 +45,31 @@ if __name__ == "__main__":
 
     args = get_parser()
     config = GetConfiguration(args.config)
+    input_files = config.get_all_input_files()
     metadata_dict = {}
     stepsize = 5000
-    with File(config.input, "r") as f:
-        njets = len(f["/jets"][:])
-    n_steps = njets//stepsize
-    global_conf = GlobalConfig()
-    for step in range(n_steps):
-        datasets = DatasetCreater(input_file=config.input, step=step, stepsize=stepsize)
-        if step == 0:
-             with File(f"{config.output}/training_topographs.h5", "w") as train_file:
-                train_file.create_dataset("Y_vertex_features", data = datasets.get_vertex_feat_y(), chunks=True, maxshape=(None,))
-                train_file.create_dataset("Y_edge_features", data = datasets.get_edge_feat_y(), chunks=True, maxshape=(None,40))
-                train_file.create_dataset("Y_edge", data = datasets.get_edge_y(), chunks=True, maxshape=(None,40))
-                train_file.create_dataset("X_train_tracks", data = datasets.get_track_input(), chunks=True, maxshape=(None,40))
-        else:
-            njets_step = datasets.get_n_valid_jets()
-            with File("training_topographs.h5", "a") as train_file:
-                train_file["Y_vertex_features"].resize((train_file["Y_vertex_features"].shape[0] + njets_step), axis=0)
-                train_file["Y_vertex_features"][-njets_step:] = datasets.get_vertex_feat_y()
-                train_file["Y_edge_features"].resize((train_file["Y_edge_features"].shape[0] + njets_step), axis=0)
-                train_file["Y_edge_features"][-njets_step:] = datasets.get_edge_feat_y()
-                train_file["Y_edge"].resize((train_file["Y_edge"].shape[0] + njets_step), axis=0)
-                train_file["Y_edge"][-njets_step:] = datasets.get_edge_y()
-                train_file["X_train_tracks"].resize((train_file["X_train_tracks"].shape[0] + njets_step), axis=0)
-                train_file["X_train_tracks"][-njets_step:] = datasets.get_track_input()
+    for input_file_ind in range(2):#len(input_files)):
+        with File(input_files[input_file_ind], "r") as f:
+            njets = len(f["/jets"][:])
+        n_steps = njets//stepsize
+        global_conf = GlobalConfig()
+        for step in range(2):#n_steps):
+            datasets = DatasetCreater(input_file=input_files[input_file_ind], step=step, stepsize=stepsize)
+            if step == 0 and input_file_ind == 0:
+                with File(f"{config.output}/training_topographs.h5", "w") as train_file:
+                    train_file.create_dataset("Y_vertex_features", data = datasets.get_vertex_feat_y(), chunks=True, maxshape=(None,))
+                    train_file.create_dataset("Y_edge_features", data = datasets.get_edge_feat_y(), chunks=True, maxshape=(None,40))
+                    train_file.create_dataset("Y_edge", data = datasets.get_edge_y(), chunks=True, maxshape=(None,40))
+                    train_file.create_dataset("X_train_tracks", data = datasets.get_track_input(), chunks=True, maxshape=(None,40))
+            else:
+                njets_step = datasets.get_n_valid_jets()
+                with File("training_topographs.h5", "a") as train_file:
+                    train_file["Y_vertex_features"].resize((train_file["Y_vertex_features"].shape[0] + njets_step), axis=0)
+                    train_file["Y_vertex_features"][-njets_step:] = datasets.get_vertex_feat_y()
+                    train_file["Y_edge_features"].resize((train_file["Y_edge_features"].shape[0] + njets_step), axis=0)
+                    train_file["Y_edge_features"][-njets_step:] = datasets.get_edge_feat_y()
+                    train_file["Y_edge"].resize((train_file["Y_edge"].shape[0] + njets_step), axis=0)
+                    train_file["Y_edge"][-njets_step:] = datasets.get_edge_y()
+                    train_file["X_train_tracks"].resize((train_file["X_train_tracks"].shape[0] + njets_step), axis=0)
+                    train_file["X_train_tracks"][-njets_step:] = datasets.get_track_input()
     
