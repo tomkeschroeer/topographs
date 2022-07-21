4cdf78617beeaa4ff5f4fa54ba851f997ddac74b
Schröer Tomke <schroeer@cpu090.yggdrasil>
2022-07-21 19:08:08 +0200

    evaluate model

---
+++ topograph/evaluate_model.py
@@ -0,0 +1,36 @@
+from puma import Histogram, HistogramPlot
+from tensorflow.keras.models import load_model
+from topograph.modules import (
+    GlobalConfig,
+    GetConfiguration,
+
+)
+from topograph.plotting import (
+    Plotter
+)
+
+def get_parser():
+    """
+    Argument parser for the train script
+
+    Returns
+    -------
+    args: parse_args
+    """
+    parser = pars.ArgumentParser()
+    parser.add_argument(
+        '--config', 
+        '-c', 
+        type=str,
+        required=True, 
+        help='config file giving the network parameters'
+    )
+
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    args = get_parser()
+    config = GetConfiguration(args.config)
+    Plotting = Plotter(config)
+
