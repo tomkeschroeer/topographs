970013aa88579b00ba3b6bae7d97ec673167d4ab
toschroe <tomke.schroer@cern.ch>
2022-08-09 11:24:14 +0200

    use field operator and adjust training

--- topograph/modules/build_network.py
+++ topograph/modules/build_network.py
@@ -1,34 +1,47 @@
+"""script to build topograph network."""
+
 from tensorflow.keras import Model
 from tensorflow.keras.layers import Layer
 
-from topograph.modules.layers import (
-    EdgeLayers,
-    FeatLayers,
-    DenseNetwork,
-    DotProduct
-)
+from topograph.modules.layers import DenseNetwork, DotProduct, EdgeLayers, FeatLayers
+
 
 class TopographModel(Layer):
     def __init__(self, config):
         self.config = config
-    
+
     def __call__(self, input_feat, input_weight):
         nodes_feat = self.config.edge_feature_network["nodes"]
         nodes_weight = self.config.edge_weight_network["nodes"]
         nodes_vertex = self.config.vertex_network["nodes"]
 
-        edge_feat_input, edge_feat_output = FeatLayers(nodes = nodes_feat, net_name="edge_feat")(input_feat) 
-        edge_weight_input, edge_weight_output_prev, edge_weight_output = EdgeLayers(nodes = nodes_weight, net_name="edge_weight")(input_weight) #EdgeLayers(nodes = nodes_weight, net_name="edge_weight")(input_weight)
-        dt_product = DotProduct(edge_weight_output,edge_feat_output)()
+        edge_feat_input, edge_feat_out = FeatLayers(
+            nodes=nodes_feat, net_name="edge_feat"
+        )(input_feat)
+        edge_wt_input, edge_wt_out_prev, edge_wt_out = EdgeLayers(
+            nodes=nodes_weight, net_name="edge_weight"
+        )(
+            input_weight
+        )  # EdgeLayers(nodes = nodes_weight, net_name="edge_weight")(input_weight)
+        dt_product = DotProduct(edge_wt_out, edge_feat_out)()
 
-        dense_vertex_output = DenseNetwork(nodes=nodes_vertex, net_name="vertex_network")(dt_product)
+        dense_vertex_out = DenseNetwork(nodes=nodes_vertex, net_name="vertex_network")(
+            dt_product
+        )
 
-        return edge_feat_input, edge_weight_input, edge_feat_output, edge_weight_output, dense_vertex_output
+        return (
+            edge_feat_input,
+            edge_wt_input,
+            edge_feat_out,
+            edge_wt_out_prev,
+            dense_vertex_out,
+        )
+
 
 def get_model(input_feat, input_weight, config):
     """
     build graph network model for b tagging
-    
+
     Parameters
     ----------
     input_feat: tuple
@@ -37,14 +50,29 @@ def get_model(input_feat, input_weight, config):
         Size of the input for the network for the edge weights
     config: object
         config defining the network parameters
-    
+
     Returns
     -------
     model : Model object
         returns graph network model
     """
-    edge_feat_input, edge_weight_input, edge_feat_output, edge_weight_output, dense_vertex_output = TopographModel(config=config)(input_feat, input_weight)
-    model = Model(inputs = [edge_feat_input, edge_weight_input], outputs = dense_vertex_output)
+    (
+        edge_feat_input,
+        edge_wt_input,
+        edge_feat_out,
+        edge_wt_out,
+        dense_vertex_out,
+    ) = TopographModel(config=config)(input_feat, input_weight)
+    model = Model(
+        inputs=[edge_feat_input, edge_wt_input],
+        outputs=[edge_wt_out, dense_vertex_out],
+    )
     model.summary()
-    model.compile(optimizer="Adam", loss="mean_squared_error")
+    model.compile(
+        optimizer="Adam",
+        loss={
+            "edge_weight_sigmoid": "binary_crossentropy",
+            "vertex_network": "mean_squared_error",
+        },
+    )
     return model
--- topograph/modules/tools.py
+++ topograph/modules/tools.py
@@ -1,23 +1,29 @@
-from h5py import File
+"""Script containing different tools."""
+import logging
+import pathlib
+from glob import glob
+
 import numpy as np
 import numpy.ma as ma
-from glob import glob
-import yaml
-import pathlib
-import logging
-
-import tensorflow as tf
 import tensorflow.keras.backend as K
-from tensorflow import constant, float32, TensorShape
+import yaml
+from h5py import File
+from tensorflow import TensorShape, constant, float32
+
 
 def step_activation(x):
-    return K.switch(x >= 0.7, constant([[1]], dtype=x.dtype), constant([[0]], dtype=x.dtype))
+    return K.switch(
+        x >= 0.7, constant([[1]], dtype=x.dtype), constant([[0]], dtype=x.dtype)
+    )
+
 
 def shifted_relu_activation(x):
-    return K.switch(x >= 0.5, 2*(x-0.5), constant([[0]], dtype=x.dtype))
+    return K.switch(x >= 0.5, 2 * (x - 0.5), constant([[0]], dtype=x.dtype))
+
 
 def Mask_invalid(x):
     return K.equal(x, np.nan)
+
 
 def get_logger():
     """Set DebugLevel for logging.
@@ -46,11 +52,13 @@ def get_logger():
     topo_logger.propagate = False
     return topo_logger
 
+
 def get_track_mask(trks):
     for var, dtype in trks.dtype.fields.items():
         if "f" in dtype[0].str:
             track_mask = ~np.isnan(trks[var])
             return track_mask
+
 
 class CustomFormatter(logging.Formatter):
     """Logging Formatter to add colors and count warning / errors
@@ -81,14 +89,18 @@ class CustomFormatter(logging.Formatter):
         formatter = logging.Formatter(log_fmt)
         return formatter.format(record)
 
+
 class GlobalConfig:
     def __init__(self):
-        global_config_path = f"{pathlib.Path(__file__).parent.absolute()}/general_config.yaml"
+        global_config_path = (
+            f"{pathlib.Path(__file__).parent.absolute()}/general_config.yaml"
+        )
         with open(global_config_path) as global_conf_file:
             global_conf = yaml.load(global_conf_file, Loader=yaml.FullLoader)
             self.track_inputs = global_conf.get("track_inputs")
             self.edge_features = global_conf.get("edge_features")
             self.vertex_features = global_conf.get("vertex_features")
+
 
 class GetConfiguration:
     def __init__(self, config_file):
@@ -98,33 +110,33 @@ class GetConfiguration:
     def getConfFile(self, config_file):
         with open(config_file, "r") as conf_file:
             self.conf = yaml.load(conf_file, Loader=yaml.FullLoader)
-  
-    def getParameters(self):
-        config_items = [
-            "input",
-            "output",
-            "stepsize",
-            "lr",
-            "njets",
-            "tracks_name",
-            "input_tracks_name",
-            "input_jet_name",
-            "input_truth_name",
-            "epochs",
-            "steps_per_epoch",
-            "edge_feature_network",
-            "edge_weight_network",
-            "vertex_network",
-            "preprocessing_file_name",
-            "one_file_name",
-            "scale_dict",
-            "training_file_name",
-            "edge_feat_name",
-            "edge_name",
-            "vertex_feat_name"
-        ]
 
-        for item in self.conf.keys():#config_items:
+    def getParameters(self):
+        # config_items = [
+        #     "input",
+        #     "output",
+        #     "stepsize",
+        #     "lr",
+        #     "njets",
+        #     "tracks_name",
+        #     "input_tracks_name",
+        #     "input_jet_name",
+        #     "input_truth_name",
+        #     "epochs",
+        #     "steps_per_epoch",
+        #     "edge_feature_network",
+        #     "edge_weight_network",
+        #     "vertex_network",
+        #     "preprocessing_file_name",
+        #     "one_file_name",
+        #     "scale_dict",
+        #     "training_file_name",
+        #     "edge_feat_name",
+        #     "edge_name",
+        #     "vertex_feat_name",
+        # ]
+
+        for item in self.conf.keys():  # config_items:
             if item in self.conf:
                 setattr(self, item, self.conf[item])
             else:
@@ -134,10 +146,11 @@ class GetConfiguration:
         try:
             return glob(self.input)
         except KeyError:
-            raise KeyError(f"No input file defined.")
+            raise KeyError("No input file defined.")
+
 
 class DatasetCreater:
-    def __init__(self, config, input_file, step, stepsize, replace_invalid = False):
+    def __init__(self, config, input_file, step, stepsize, replace_invalid=False):
         self.global_conf = GlobalConfig()
         self.config = config
         self.input_file = input_file
@@ -147,10 +160,18 @@ class DatasetCreater:
         self.replace_invalid = replace_invalid
 
         with File(self.input_file, "r") as f:
-            self.truth = f[f"/{self.config.input_truth_name}"][self.step*self.stepsize:(self.step+1)*self.stepsize :]
-            self.HadrConeTruth = f[f"/{self.config.input_jet_name}"].fields("HadronConeExclExtendedTruthLabelID")[self.step*self.stepsize:(self.step+1)*self.stepsize][:]
-            self.reco = f[f"/{self.config.input_tracks_name}"][self.step*self.stepsize:(self.step+1)*self.stepsize, :]
-            self.edge_features = f["/edge_features"][self.step*self.stepsize:(self.step+1)*self.stepsize, :]
+            self.truth = f[f"/{self.config.input_truth_name}"][
+                self.step * self.stepsize : (self.step + 1) * self.stepsize
+            ]
+            self.HadrConeTruth = f[f"/{self.config.input_jet_name}"].fields(
+                "HadronConeExclExtendedTruthLabelID"
+            )[self.step * self.stepsize : (self.step + 1) * self.stepsize][:]
+            self.reco = f[f"/{self.config.input_tracks_name}"][
+                self.step * self.stepsize : (self.step + 1) * self.stepsize, :
+            ]
+            self.edge_features = f["/edge_features"][
+                self.step * self.stepsize : (self.step + 1) * self.stepsize, :
+            ]
 
         self.ind_truthflav = self.get_b_indeces()
         self.truth = self.truth[self.ind_truthflav]
@@ -159,54 +180,89 @@ class DatasetCreater:
 
     def get_b_indeces(self):
         hadronflavour = self.truth["flavour"]
-        return np.logical_and(self.HadrConeTruth == 5, [sum(hf == 5)==1 for hf in hadronflavour])
-   
-    def get_n_valid_jets(self): 
+        return np.logical_and(
+            self.HadrConeTruth == 5, [sum(hf == 5) == 1 for hf in hadronflavour]
+        )
+
+    def get_n_valid_jets(self):
         return sum(self.ind_truthflav)
 
     def get_edge_y(self):
-        truthOriginLabel = self.edge_features["truthOriginLabel"]
-        tOL_fromB = [[OL == 3 for OL in tracklabels] for tracklabels in truthOriginLabel]
-        tOL_fromBC = [[OL == 4 for OL in tracklabels] for tracklabels in truthOriginLabel]
-        tOL = np.array([[np.array([edge_y]).astype(int) for edge_y in (np.logical_or(fromB, fromBC))] for fromB, fromBC in zip(tOL_fromB, tOL_fromBC)])
+        truthOriginLabel = self.edge_features.fields("truthOriginLabel")
+        tOL_fromB = [
+            [OL == 3 for OL in tracklabels] for tracklabels in truthOriginLabel
+        ]
+        tOL_fromBC = [
+            [OL == 4 for OL in tracklabels] for tracklabels in truthOriginLabel
+        ]
+        tOL = np.array(
+            [
+                [
+                    np.array([edge_y]).astype(int)
+                    for edge_y in (np.logical_or(fromB, fromBC))
+                ]
+                for fromB, fromBC in zip(tOL_fromB, tOL_fromBC)
+            ]
+        )
         return tOL
-    
+
     def get_edge_feat_y(self):
-        edge_feat_y = np.array([[list(feat_track) for feat_track in feat_jet] for feat_jet in self.edge_features[self.global_conf.edge_features]])
+        edge_feat_y = np.array(
+            [
+                [list(feat_track) for feat_track in feat_jet]
+                for feat_jet in self.edge_features.fields(
+                    self.global_conf.edge_features
+                )
+            ]
+        )
         return edge_feat_y
 
     def get_vertex_feat_y(self):
-        vertex_feat = np.array([list(vertex_feat[hf == 5][0]) for hf, vertex_feat in zip(self.truth["flavour"], self.truth[self.global_conf.vertex_features])])
+        vertex_feat = np.array(
+            [
+                list(vertex_feat[hf == 5][0])
+                for hf, vertex_feat in zip(
+                    self.truth.fields("flavour"),
+                    self.truth.fields(self.global_conf.vertex_features),
+                )
+            ]
+        )
         return vertex_feat
-       
+
     def get_track_input(self):
-        track_input = ma.masked_invalid([[list(inputs_track) for inputs_track in input_jet] for input_jet in self.reco])
+        track_input = ma.masked_invalid(
+            [
+                [list(inputs_track) for inputs_track in input_jet]
+                for input_jet in self.reco
+            ]
+        )
         mask = track_input.mask
         track_input[mask] = -999
         return track_input
 
+
 class DataGenerator:
     def __init__(
         self,
-        input : str,
-        metadata_dict : dict,
+        input: str,
+        metadata_dict: dict,
         get_labels: bool = True,
         get_inputs: bool = True,
         get_weight_labels: bool = False,
-        stepsize : int = 5000,
-        savejets : bool = False,
-        savetracks : bool = True,
-        track_name : str = "tracks",
-        edge_name : str ="edge",
-        edge_feat_name : str = "edge_feat",
-        vertex_feat_name : str = "vertex_feat"
-        ):
+        stepsize: int = 5000,
+        savejets: bool = False,
+        savetracks: bool = True,
+        track_name: str = "tracks",
+        edge_name: str = "edge",
+        edge_feat_name: str = "edge_feat",
+        vertex_feat_name: str = "vertex_feat",
+    ):
         """
         class to get dataset for topograph training
 
         Parameters
         ----------
-        input: 
+        input:
             input file containing the jet and track information
         metadata_dict:
             dictionary containing the total number of jets and tracks
@@ -228,56 +284,122 @@ class DataGenerator:
         self.edge_name = edge_name
         self.vertex_feat_name = vertex_feat_name
 
-    def load_in_memory(self, step : int = 0):
+    def load_in_memory(self, step: int = 0):
         with File(self.input) as f:
-            #self.edge_feat_batch = f[self.edge_feat_name][step*self.stepsize : (step+1)*self.stepsize]
+            # self.edge_feat_batch = f[self.edge_feat_name][step*self.stepsize : (step+1)*self.stepsize]
             if self.get_inputs:
-                self.track_batch = f[self.track_name][step*self.stepsize : (step+1)*self.stepsize]
+                self.track_batch = f[self.track_name][
+                    step * self.stepsize : (step + 1) * self.stepsize
+                ]
             if self.get_labels:
-                self.vertex_feat_batch = f[self.vertex_feat_name][step*self.stepsize : (step+1)*self.stepsize]
+                self.vertex_feat_batch = f[self.vertex_feat_name][
+                    step * self.stepsize : (step + 1) * self.stepsize
+                ]
             if self.get_weight_labels:
-                self.edge_batch = f[self.edge_name][step*self.stepsize : (step+1)*self.stepsize]
-    
+                self.edge_batch = f[self.edge_name][
+                    step * self.stepsize : (step + 1) * self.stepsize
+                ]
+
     def get_types_shapes(self):
-        if self.get_labels and self.get_inputs:
-            types = ({
-                    "input_1": float32,
-                    "input_2": float32
+        if self.get_labels and self.get_inputs and self.get_weight_labels:
+            types = (
+                {"input_1": float32, "input_2": float32},
+                {"edge_weight_sigmoid": float32, "vertex_network": float32},
+            )
+            shapes = (
+                {
+                    "input_1": TensorShape(
+                        (
+                            None,
+                            self.metadata_dict["n_trks"],
+                            self.metadata_dict["n_trk_features"],
+                        )
+                    ),
+                    "input_2": TensorShape(
+                        (
+                            None,
+                            self.metadata_dict["n_trks"],
+                            self.metadata_dict["n_trk_features"],
+                        )
+                    ),
                 },
-                float32)
-            shapes = ({
-                    "input_1": TensorShape((None, self.metadata_dict["n_trks"], self.metadata_dict["n_trk_features"])),
-                    "input_2": TensorShape((None, self.metadata_dict["n_trks"], self.metadata_dict["n_trk_features"]))
+                {
+                    "edge_weight_sigmoid": TensorShape(
+                        (
+                            None,
+                            self.metadata_dict["n_trks"],
+                            self.metadata_dict["n_edge_y"],
+                        )
+                    ),
+                    "vertex_network": TensorShape(
+                        (None, self.metadata_dict["n_vertex_feat"])
+                    ),
                 },
-                TensorShape((None, self.metadata_dict["n_vertex_feat"]))
+            )
+        elif self.get_labels and self.get_inputs:
+            types = ({"input_1": float32, "input_2": float32}, float32)
+            shapes = (
+                {
+                    "input_1": TensorShape(
+                        (
+                            None,
+                            self.metadata_dict["n_trks"],
+                            self.metadata_dict["n_trk_features"],
+                        )
+                    ),
+                    "input_2": TensorShape(
+                        (
+                            None,
+                            self.metadata_dict["n_trks"],
+                            self.metadata_dict["n_trk_features"],
+                        )
+                    ),
+                },
+                TensorShape((None, self.metadata_dict["n_vertex_feat"])),
             )
         elif self.get_labels:
-            types = (float32)
-            shapes = (TensorShape((None, self.metadata_dict["n_vertex_feat"])))
+            types = float32
+            shapes = TensorShape((None, self.metadata_dict["n_vertex_feat"]))
         elif self.get_inputs:
-            types = ({
-                    "input_1": float32,
-                    "input_2": float32
-                })
-            shapes = ({
-                    "input_1": TensorShape((None, self.metadata_dict["n_trks"], self.metadata_dict["n_trk_features"])),
-                    "input_2": TensorShape((None, self.metadata_dict["n_trks"], self.metadata_dict["n_trk_features"]))
-                })
+            types = {"input_1": float32, "input_2": float32}
+            shapes = {
+                "input_1": TensorShape(
+                    (
+                        None,
+                        self.metadata_dict["n_trks"],
+                        self.metadata_dict["n_trk_features"],
+                    )
+                ),
+                "input_2": TensorShape(
+                    (
+                        None,
+                        self.metadata_dict["n_trks"],
+                        self.metadata_dict["n_trk_features"],
+                    )
+                ),
+            }
         return types, shapes
-        
+
 
 class DataLoader(DataGenerator):
     def __call__(self):
         n_samples = self.metadata_dict["n_jets"]
-        n_steps = n_samples//self.stepsize +1
+        n_steps = n_samples // self.stepsize + 1
         for step in range(n_steps):
             self.load_in_memory(step=step)
-            if self.get_inputs and self.get_labels and not self.get_weight_labels:
-                yield {"input_1":self.track_batch, "input_2":self.track_batch}, self.vertex_feat_batch
+            if self.get_inputs and self.get_labels and self.get_weight_labels:
+                yield {"input_1": self.track_batch, "input_2": self.track_batch}, {
+                    "edge_weight_sigmoid": self.edge_batch,
+                    "vertex_network": self.vertex_feat_batch,
+                }
+            elif self.get_inputs and self.get_labels and not self.get_weight_labels:
+                yield {
+                    "input_1": self.track_batch,
+                    "input_2": self.track_batch,
+                }, self.vertex_feat_batch
             elif self.get_inputs:
-                yield {"input_1":self.track_batch, "input_2":self.track_batch}
+                yield {"input_1": self.track_batch, "input_2": self.track_batch}
             elif self.get_labels:
                 yield self.vertex_feat_batch
             elif self.get_weight_labels:
                 yield self.get_weight_labels
-
