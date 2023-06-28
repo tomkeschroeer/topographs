2f63716c4fc869c315a93ffc1d7b926fab399510
toschroe <tomke.schroer@cern.ch>
2023-06-28 09:24:42 +0200

    adjust plotting to dataset changes

--- topograph/plotting/plotting_modules.py
+++ topograph/plotting/plotting_modules.py
@@ -153,16 +153,12 @@ def get_predictions_and_labels(model, dataset):
     for sample in dataset:
         inputs, labels_edge, labels_vertex, sample_weights, mask, mask_vertex = sample
         labels_shape = labels_edge.size()
-        print(labels_shape)
         # labels_edge = labels_edge.reshape(labels_shape[1], labels_shape[2], labels_shape[0])
         sample_weight_shape = sample_weights.size()
-        print(sample_weight_shape)
         # sample_weights = sample_weights.reshape(sample_weight_shape[1], sample_weight_shape[2], sample_weight_shape[0])
         labels_vertex_shape = labels_vertex.size()
-        print(labels_vertex_shape)
         # labels_vertex = labels_vertex.reshape(labels_vertex_shape[1], labels_vertex_shape[2])
         inputs_shape = inputs.size()
-        print(inputs_shape)
         # inputs = inputs.reshape(inputs_shape[1], inputs_shape[2], inputs_shape[3])
         # mask_shape = mask.size()
         # mask = mask.reshape(mask_shape[1], mask_shape[2])
@@ -184,23 +180,19 @@ def get_predictions_and_labels(model, dataset):
     shape_preds_v = np.array(preds_v).shape
     shape_masks = np.array(masks).shape
     shape_grads = np.array(grads).shape
-    print(shape_labels_e)
-    print(shape_labels_v)
-    print(shape_preds_e)
-    print(shape_preds_v)
     preds_e = np.array(preds_e).reshape(shape_preds_e[0]*shape_preds_e[1],shape_preds_e[2])#,*shape_preds_e[3:])
     preds_v = np.array(preds_v).reshape(shape_preds_v[0]*shape_preds_v[1],*shape_preds_v[2:])#,*shape_preds_v[3:])
     labels_e = np.array(labels_e).reshape(shape_labels_e[0]*shape_labels_e[1],shape_labels_e[2])
     labels_v = np.array(labels_v).reshape(shape_labels_v[0]*shape_labels_v[1],*shape_labels_v[2:])
     # masks = np.array(masks).reshape(shape_masks[0]*shape_masks[1],*shape_masks[2:])
-    grads = np.array(grads).reshape(shape_grads[0]*shape_grads[1]*shape_grads[2],*shape_grads[3:])
+    grads = np.array(grads).reshape(shape_grads[0]*shape_grads[1],*shape_grads[2:])
     return preds_e, preds_v, labels_e, labels_v, masks, grads
 
 class Plotter:
     def __init__(self, config, cut_val=None, vars=None):
         self.config = config
         self.cut_val = cut_val
-        self.global_config = GlobalConfig(alternative_conf="general_config_plot")
+        self.global_config = GlobalConfig(alternative_conf=None)
         if cut_val is not None:
             self.cut_val = cut_val if cut_val <= 1 else cut_val/100
         self.logger = get_logger()
