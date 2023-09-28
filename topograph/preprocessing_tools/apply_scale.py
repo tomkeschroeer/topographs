correct merging and scaling

topograph/preprocessing_tools/apply_scale.py
@@ -26,6 +26,7 @@ class Apply_Scaler:
         }
         self.njets = self.config.njets + self.config.njets_val + self.config.njets_test
         self.out_file = None
+        self.jet_types = self.config.jet_types
 
     def Run(self):
         logger = get_logger()
@@ -60,94 +61,97 @@ class Apply_Scaler:
         # for file_name, njets_per_file in self.file_names.items():
         self.out_file = input_file.replace(".h5", "_scaled.h5")
         logger.info(f"Save scaled inputs in file {self.out_file}")
-        # with File(self.out_file, "w") as h5file:
-        #     for keyname in dict_names:
-        #         scale_generator = self.scale_generator(
-        #             input_file=input_file,
-        #             nJets=self.njets,
-        #             keyname=keyname,
-        #             scale_dict=scale_dict[keyname],
-        #             chunk_size=chunk_size,
-        #         )
-        #         # Set up chunk counter and start looping
-        #         chunk_counter = 0
-        #         for chunk_counter in range(n_chunks):
-        #             logger.info(
-        #                 f"Applying scales for chunk {chunk_counter+1} of"
-        #                 f" {n_chunks}."
-        #             )
-        #             try:
-        #                 data = next(scale_generator)
+        with File(self.out_file, "w") as h5file:
+            for keyname in dict_names:
+                scale_generator = self.scale_generator(
+                    input_file=input_file,
+                    nJets=self.njets,
+                    keyname=keyname,
+                    scale_dict=scale_dict[keyname],
+                    chunk_size=chunk_size,
+                )
+                # Set up chunk counter and start looping
+                chunk_counter = 0
+                for chunk_counter in range(n_chunks):
+                    logger.info(
+                        f"Applying scales for chunk {chunk_counter+1} of"
+                        f" {n_chunks}."
+                    )
+                    try:
+                        data = next(scale_generator)
 
-        #                 if chunk_counter == 0:
-        #                     h5file.create_dataset(
-        #                         keyname,
-        #                         data=data[0],
-        #                         #  compression="lzf",
-        #                         chunks=((100,) + data[0].shape[1:]),
-        #                         maxshape=(
-        #                             None,
-        #                             *(data[0].shape[1:]),
-        #                         ),
-        #                     )
+                        if chunk_counter == 0:
+                            h5file.create_dataset(
+                                keyname,
+                                data=data[0],
+                                #  compression="lzf",
+                                chunks=((100,) + data[0].shape[1:]),
+                                maxshape=(
+                                    None,
+                                    *(data[0].shape[1:]),
+                                ),
+                            )
 
-        #                 else:
-        #                     h5file[keyname].resize(
-        #                         (h5file[keyname].shape[0] + data[0].shape[0]),
-        #                         axis=0,
-        #                     )
+                        else:
+                            h5file[keyname].resize(
+                                (h5file[keyname].shape[0] + data[0].shape[0]),
+                                axis=0,
+                            )
 
-        #                     h5file[keyname][-data[0].shape[0] :] = data[0]
+                            h5file[keyname][-data[0].shape[0] :] = data[0]
 
-        #             except StopIteration:
-        #                 break
+                    except StopIteration:
+                        break
 
-        #             chunk_counter += 1
+                    chunk_counter += 1
 
-        # self.save_remaining_dt(logger, input_file, n_entries_total=self.njets)
-        # chunk_size= 870
+        self.save_remaining_dt(logger, input_file, n_entries_total=self.njets)
+        # chunk_size = np.min() 
         starting_point = 0
         n_total = len(File(self.out_file,"r")[f"/{self.config.tracks_name}"])
         ind_array = np.linspace(0, n_total-1, n_total).astype(int)
         scary_shuffle(ind_array)
         n_jets_per_jettype = {jettype: 0 for jettype in self.jet_types}
-        total_njets_per_jettype = int(njets/len(self.jet_types))
-        for jet_type in self.jet_types:
-            for file_name, njets in self.file_names.items():
-                input_file = (
-                    f"{self.config.output}/{file_name}".replace(
-                        ".h5", ""
-                    )
-                    + ".h5"
+        total_njets_per_jettype = int(self.njets/len(self.jet_types))
+        starting_point = 0
+        for file_name, njets in self.file_names.items():
+            input_file = (
+                f"{self.config.output}/{file_name}".replace(
+                    ".h5", ""
                 )
+                + ".h5"
+            )
+            with File(input_file, "w") as f:
+                nsteps = int(njets//chunk_size)
                 r = njets % chunk_size
                 if r != 0:
-                    chunk_size = chunk_size - r
-                nsteps = int(n_total//chunk_size)
+                    nsteps += 1
+                print(nsteps)
                 # starting_point = 0
                 inds = np.array([[(step*chunk_size + starting_point), ((step+1)*chunk_size + starting_point)] for step in range(nsteps)], dtype=int)
-                if inds[-1,1] > njets: inds[-1,1] = njets + starting_point
+                if inds[-1,1] > njets + starting_point: inds[-1,1] = njets + starting_point
                 starting_point = inds[-1,1]
                 create_file = True
-                with File(self.out_file, "r") as o:
-                    keys=o.keys()
-                    print(keys)
-                    with File(input_file, "w") as f:
+                for jet_type in self.jet_types:
+                    with File(self.out_file, "r") as o:
+                        keys=o.keys()
                         for ind in inds:
-                            jet_type = o["jet_type"][ind[0]:ind[1]]
-                            mask = (jet_type == self.jet_types.index(jet_type))
+                            jet_type_in_jet = o["jet_type"][ind[0]:ind[1]]
+                            # print(self.jet_types.index(jet_type))
+                            mask = (jet_type_in_jet == self.jet_types.index(jet_type))
                             njets_step = sum(mask)
                             n_jets_per_jettype[jet_type] = n_jets_per_jettype[jet_type] + njets_step
                             # if n_jets_per_jettype[jet_type] >= total_njets_per_jettype:
                             if create_file:
                                 for key in keys:
                                     data_chunk = o[key][ind[0]:ind[1]][mask]
-                                    print("data_chunk ready")
                                     shape = data_chunk.shape
                                     shape = (None,) if len(shape) == 1 else (None, *shape[1:])
                                     f.create_dataset(key, data=data_chunk, chunks=True, maxshape=shape)
                                 create_file = False
                             else:
+                                print("in else")
+                                # print(o[key][ind[0]:ind[1]])
                                 for key in keys: #ind[1]-ind[0]
                                     f[key].resize((f[key].shape[0] + njets_step), axis=0)
                                     f[key][-njets_step:] = o[key][ind[0]:ind[1]][mask]
@@ -198,7 +202,6 @@ class Apply_Scaler:
 
         # Open the file and load the jets
         with File(input_file, "r") as f:
-
             # Get the indices
             start_ind = 0
             tupled_indices = []

topograph/preprocessing_tools/merge.py
@@ -32,6 +32,7 @@ class Merge:
             + self.dataset_types["_test"]["njets"]
         )/len(jet_types))
         stepsize = min(50_000, int(njets / 2))
+        print(stepsize)
         n_steps = njets // stepsize + 1
         array_full={}
         array_dict_labels_full={}
@@ -41,7 +42,7 @@ class Merge:
         njets_dict = {jet_type: len(File(f"{output_file}_{jet_type}.h5", "r")[f"{self.config.edge_name}_{jet_type}"]) for jet_type in jet_types}
         non_stack_keys = [self.config.edge_name, self.config.vertex_feat_name]
         non_stack_keys_for_check = [f"{self.config.edge_name}_{jet_types[0]}", f"{self.config.vertex_feat_name}_{jet_types[0]}"]
-        keys = [key for key in File(f"{output_file}_{jet_types[0]}.h5").keys() if key not in non_stack_keys_for_check]
+        keys = [key for key in File(f"{output_file}_{jet_types[0]}.h5").keys()]# if key not in non_stack_keys_for_check]
         with File(
             f"{output_file}.h5", "w"
         ) as h5fw:
@@ -56,23 +57,23 @@ class Merge:
                         array_dict[f"jet_type_{jet_type}"] = np.full(shape=(len(array_dict[f"{keys[0]}_{jet_type}"])), fill_value=i)
                         array_dict[f"truthorigin_{jet_type}"] = truthorigin
                         array_full.update(array_dict)
-                        for jet_type_2 in jet_types:
-                            if jet_type_2 == jet_type:
-                                array_dict_labels = {f"{key}_{jet_type}_{jet_type_2}": h5fr[f"/{key}_{jet_type}"][step*stepsize:(step+1)*stepsize] for key in non_stack_keys}
-                                array_dict_labels_full.update(array_dict_labels)
-                            else:
-                                with File(f"{output_file}_{jet_type_2}.h5", "r") as h5fr_2:
-                                    y_edge = h5fr_2[f"/{self.config.edge_name}_{jet_type_2}"][step*stepsize:(step+1)*stepsize]
-                                    y_vertex = h5fr_2[f"/{self.config.vertex_feat_name}_{jet_type_2}"][step*stepsize:(step+1)*stepsize]
-                                array_dict_labels = {
-                                    f"{self.config.edge_name}_{jet_type}_{jet_type_2}": np.full(shape=y_edge.shape, dtype=y_edge.dtype, fill_value=0),
-                                    f"{self.config.vertex_feat_name}_{jet_type}_{jet_type_2}": np.full(shape=y_vertex.shape, dtype=y_vertex.dtype, fill_value=-999.),
-                                }
-                                array_dict_labels_full.update(array_dict_labels)
+                        # for jet_type_2 in jet_types:
+                            #  if jet_type_2 == jet_type:
+                            # array_dict_labels = {f"{key}_{jet_type}_{jet_type_2}": h5fr[f"/{key}_{jet_type}"][step*stepsize:(step+1)*stepsize] for key in non_stack_keys}
+                            # array_dict_labels_full.update(array_dict_labels)
+                            # else:
+                            #     with File(f"{output_file}_{jet_type_2}.h5", "r") as h5fr_2:
+                            #         y_edge = h5fr_2[f"/{self.config.edge_name}_{jet_type_2}"][step*stepsize:(step+1)*stepsize]
+                            #         y_vertex = h5fr_2[f"/{self.config.vertex_feat_name}_{jet_type_2}"][step*stepsize:(step+1)*stepsize]
+                            #     array_dict_labels = {
+                            #         f"{self.config.edge_name}_{jet_type}_{jet_type_2}": np.full(shape=y_edge.shape, dtype=y_edge.dtype, fill_value=0),
+                            #         f"{self.config.vertex_feat_name}_{jet_type}_{jet_type_2}": np.full(shape=y_vertex.shape, dtype=y_vertex.dtype, fill_value=-999.),
+                            #     }
+                            #     array_dict_labels_full.update(array_dict_labels)
                     # array_dict_comb_labels = {key: np.concatenate([array_dict_labels_full[f"{key}_{jet_type}_{jet_type_2}"] for jet_type_2 in jet_types]) for key in non_stack_keys}
                     array_dict_comb_labels = {}
-                    for key in non_stack_keys:
-                        array_dict_comb_labels[f"{key}_{jet_type}"] = np.concatenate([array_dict_labels_full[f"{key}_{jet_type}_{jet_type_2}"] for jet_type_2 in jet_types])
+                    # for key in non_stack_keys:
+                    #     array_dict_comb_labels[f"{key}_{jet_type}"] = np.concatenate([array_dict_labels_full[f"{key}_{jet_type}_{jet_type_2}"] for jet_type_2 in jet_types])
                     array_dict_comb_labels_full.update(array_dict_comb_labels)
                 # for jet_type in jet_types:
                 #     array_full[f"jet_type_{jet_types}"] = np.full(shape=(array_full[f"{key}_{jet_type}"]))
@@ -114,6 +115,8 @@ class Merge:
                 #     for key in array_dict_comb.keys():
                 #         array_dict_comb[key] = np.delete(array_dict_comb[key],comm_c, axis=0)
                 nentries = len(next(iter(array_dict_comb.values())))
+                # print(nentries)
+                # print(array_dict_comb["Y_vertex_features_c"])
                 indices = np.linspace(0, nentries-1, nentries).astype(int)
                 scary_shuffle(indices)
                 for key in array_dict_comb.keys():

topograph/preprocessing_tools/scale.py
@@ -22,68 +22,66 @@ class Scaler:
         chunk_size = 1e5
         scale_dict_trk = {}
         scale_dict_trk_selection = {}
-        for i, jet_type in enumerate(self.config.jet_types):
-            print(jet_type)
-            file_name = (
-                f"{self.config.output}/{self.config.preprocessing_file_name}_{jet_type}".replace(
-                # f"{self.config.output}/{self.config.preprocessing_file_name}_{jet_type}".replace(
-                    ".h5", ""
+        file_name = (
+            # f"{self.config.output}/{self.config.preprocessing_file_name}_{jet_type}".replace(
+            f"{self.config.output}/{self.config.preprocessing_file_name}".replace(
+                ".h5", ""
+            )
+            + ".h5"
+        )
+        self.scale_dict_path = (
+            f"{self.config.output}/{self.config.scale_dict}".replace(".json", "")
+            # f"{self.config.output}/{self.config.scale_dict}_{jet_type}".replace(".json", "")
+            + ".json"
+        )
+        # Get the file_length
+        file_length = len(File(file_name, "r")[f"/{self.config.tracks_name}"])
+
+        # Get the number of chunks we need to load
+        n_chunks = int(np.ceil(file_length / chunk_size))
+        if n_chunks == 0:
+            n_chunks = 1
+            chunk_size = file_length
+
+        logger.info("Calculating scaling and shifting values for the track variables")
+        
+        # Load generator
+        scaling_generator = self.get_scaling_generator(
+            input_file=file_name,
+            nJets=file_length,
+            tracks_name=self.config.tracks_name,
+            vert_prop_name=self.config.vertex_feat_name,
+            chunk_size=chunk_size,
+        )
+
+        # Loop over chunks
+        for chunk_counter in range(n_chunks):
+            logger.info(f"Using chunk {chunk_counter+1} from {n_chunks}")
+            # Check if this is the first time loading from the generator
+            if chunk_counter == 0:
+                # Get the first chunk of scales from the generator
+                scale_dict_selection, nEntries_loaded = next(scaling_generator)
+            else:
+                # Get the next chunk of scales from the generator
+                tmp_dict, tmp_nEntries_loaded = next(scaling_generator)
+
+                # Combine the scale dicts coming from the generator
+                (scale_dict_selection, nEntries_loaded,) = self.join_scale_dicts_trks(
+                    first_scale_dict=scale_dict_selection,
+                    second_scale_dict=tmp_dict,
+                    first_ns=nEntries_loaded,
+                    second_ns=tmp_nEntries_loaded,
                 )
-                + ".h5"
-            )
-            self.scale_dict_path = (
-                f"{self.config.output}/{self.config.scale_dict}".replace(".json", "")
-                f"{self.config.output}/{self.config.scale_dict}_{jet_type}".replace(".json", "")
-                + ".json"
-            )
-            # Get the file_length
-            file_length = len(File(file_name, "r")[f"/{self.config.tracks_name}"])
 
-            # Get the number of chunks we need to load
-            n_chunks = int(np.ceil(file_length / chunk_size))
-            if n_chunks == 0:
-                n_chunks = 1
-                chunk_size = file_length
+        scale_dict_trk.update({self.config.input_tracks_name: scale_dict_selection})
 
-            logger.info("Calculating scaling and shifting values for the track variables")
-           
-            # Load generator
-            scaling_generator = self.get_scaling_generator(
-                input_file=file_name,
-                nJets=file_length,
-                tracks_name=self.config.tracks_name,
-                vert_prop_name=self.config.vertex_feat_name,
-                chunk_size=chunk_size,
-            )
-
-            # Loop over chunks
-            for chunk_counter in range(n_chunks):
-                logger.info(f"Using chunk {chunk_counter+1} from {n_chunks}")
-                # Check if this is the first time loading from the generator
-                if chunk_counter == 0 and i == 0:
-                    # Get the first chunk of scales from the generator
-                    scale_dict_selection, nEntries_loaded = next(scaling_generator)
-                else:
-                    # Get the next chunk of scales from the generator
-                    tmp_dict, tmp_nEntries_loaded = next(scaling_generator)
-
-                    # Combine the scale dicts coming from the generator
-                    (scale_dict_selection, nEntries_loaded,) = self.join_scale_dicts_trks(
-                        first_scale_dict=scale_dict_selection,
-                        second_scale_dict=tmp_dict,
-                        first_ns=nEntries_loaded,
-                        second_ns=tmp_nEntries_loaded,
-                    )
-
-            scale_dict_trk.update({self.config.input_tracks_name: scale_dict_selection})
-
-            # Add scale dict for given tracks selection to the more general one
-            # TODO: change in python 3.9
-            # save scale/shift dictionary to json file
-            os.makedirs(os.path.dirname(self.scale_dict_path), exist_ok=True)
-            with open(f"{self.scale_dict_path}", "w") as outfile:
-                json.dump(scale_dict_selection, outfile, indent=4)
-            logger.info(f"Saved scale dictionary as {self.scale_dict_path}")
+        # Add scale dict for given tracks selection to the more general one
+        # TODO: change in python 3.9
+        # save scale/shift dictionary to json file
+        os.makedirs(os.path.dirname(self.scale_dict_path), exist_ok=True)
+        with open(f"{self.scale_dict_path}", "w") as outfile:
+            json.dump(scale_dict_selection, outfile, indent=4)
+        logger.info(f"Saved scale dictionary as {self.scale_dict_path}")
 
     def get_scaling_generator(
         self,
@@ -151,8 +149,10 @@ class Scaler:
                 # )
                 track_mask = get_mask(tracks_chunk)
                 vert_mask = {jet_type: get_mask(vert_prop_chunk[jet_type]) for jet_type in self.config.jet_types}
-                for i in vert_mask.values():
-                    print(np.unique(i))
+                for jet_type in self.config.jet_types:
+                    print(jet_type)
+                    print(sum(vert_prop_chunk[jet_type]["pt"]!=-999.))
+                    print(sum(vert_mask[jet_type]))
                 # vert_mask_b = get_mask(vert_prop_chunk_b)
                 # vert_mask_c = get_mask(vert_prop_chunk_c)
 